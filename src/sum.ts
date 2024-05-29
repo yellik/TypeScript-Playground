64753ab13ef22c36d82ebba0b51515db4424223f
Yellik <yelenakensborn@gmail.com>
2024-05-29 18:57:54 +0200

    dev env ts jest

---
+++ src/sum.ts
@@ -0,0 +1,10 @@
+const sum = (a : number, b: number) => {
+    return a + b;
+}
+
+const difference = (a: number) => {
+    return 
+}
+export {
+    sum
+}
---
+++ src/tests/som.test.js
@@ -0,0 +1,7 @@
+it("returns a sum of two integers", () => {
+    expect(1 + 2).toEqual(3);
+})
+
+it("should return an absolute difference between a number and 19", () => {
+    expect(20).toBeTruthy();
+})
